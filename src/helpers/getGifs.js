a803cea730be345cf4d73e12679f9f55a0c3fad1
Yeison Tobo <yeisontobo5@gmail.com>
2024-04-12 21:27:32 -0500

    first commit

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=ulGsercIqPvCY1DdmbKmxfcGQC4Ymdbj&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
